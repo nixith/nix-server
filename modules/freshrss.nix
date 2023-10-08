1d9e6f1ad7033fa66342f6ac5186354898820548
bromine1 <rwstauffer72@gmail.com>
2023-10-08 19:47:27 -0400

    set passwordFile explicitly

--- modules/freshrss.nix
+++ modules/freshrss.nix
@@ -1,8 +1,9 @@
-{...}: {
+{config, ...}: {
   sops.secrets.freshrss = {
     owner = "freshrss";
   };
   services.freshrss = {
+    passwordFile = config.sops.secrets.freshrss.path;
     baseUrl = "https://patchouli:2120";
     enable = true;
   };
