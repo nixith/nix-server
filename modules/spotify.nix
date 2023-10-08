a9f34c706dcd7181b20db5c07a27959725ce4d0a
bromine1 <rwstauffer72@gmail.com>
2023-10-08 13:52:48 -0400

    use systemd credentials

--- modules/spotify.nix
+++ modules/spotify.nix
@@ -1,30 +1,34 @@
-{config, ...}: let
+  services.spotifyd = {config, ...}: let
   usernamePath = "/var/lib/spotifyd/username";
   passwordPath = "/var/lib/spotifyd/password";
 in {
   sops.secrets."spotifyd/password" = {
     path = passwordPath;
-    group = "spotifyd";
-    mode = "0440";
+    #  group = "spotifyd";
+    #  mode = "0440";
   };
   sops.secrets."spotifyd/username" = {
     path = usernamePath;
-    group = "spotifyd";
-    mode = "0440";
+    #  group = "spotifyd";
+    #  mode = "0440";
   };
-  users.extraGroups."spotifyd" = {};
-  services.spotifyd = {
+  systemd.services.spotifyd.serviceConfig.LoadCredential = [
+    "username:${usernamePath}"
+    "password:${passwordPath}"
+  ];
+
+services.spotifyd = {
     enable = true;
     settings = {
       global = {
         audio_format = "S16";
         autoplay = true;
-        username_cmd = "cat ${usernamePath}";
-        password_cmd = "cat ${passwordPath}";
+        username_cmd = "cat $CREDENTIALS_DIRECTORY/username";
+        password_cmd = "cat $CREDENTIALS_DIRECTORY/password";
         backend = "pulseaudio";
         bitrate = 320;
         cache_path = "cache_directory";
-        dbus_type = "system";
+        #dbus_type = "system";
         device = "default";
         device_name = "patchouli";
         device_type = "computer";
@@ -33,10 +37,10 @@ in {
         mixer = "PCM";
         no_audio_cache = true;
         normalisation_pregain = -10;
-        on_song_change_hook = "command_to_run_on_playback_events";
+        #on_song_change_hook = "command_to_run_on_playback_events";
         # proxy = "http://proxy.example.org:8080";
-        use_keyring = true;
-        use_mpris = true;
+        #use_keyring = true;
+        use_mpris = false;
         volume_controller = "alsa";
         volume_normalisation = true;
         # zeroconf_port = 1234;
