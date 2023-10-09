cf0548706f9a75429e567308610b75cb99c103be
bromine1 <rwstauffer72@gmail.com>
2023-10-08 23:48:55 -0400

    move to seperate ports

--- modules/rss.nix
+++ modules/rss.nix
@@ -8,9 +8,21 @@
     enable = true;
     defaultUser = "admin";
   };
+  services.nginx.virtualHosts."freshrss".listen = [
+    {
+      port = 2020;
+      addr = "0.0.0.0";
+    }
+  ];
 
   services.rss-bridge = {
     enable = true;
     whitelist = ["*"];
   };
+  services.nginx.virtualHosts."rss-bridge".listen = [
+    {
+      port = 2021;
+      addr = "0.0.0.0";
+    }
+  ];
 }
