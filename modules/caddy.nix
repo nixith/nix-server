a815f7a1284b6aa7faafb51003cc4c6e49e1d2ed
bromine1 <rwstauffer72@gmail.com>
2023-10-09 00:30:34 -0400

    add caddy reverse proxy

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -1,5 +1,18 @@
 {...}: {
   services.caddy = {
     enable = true;
+    virtualHosts."proxy" = {
+      listenAddresses = [
+        "0.0.0.0"
+        "::0"
+      ];
+      serverAliases = ["reverse-proxy"];
+      extraConfig = ''
+
+        handle /freshrss/* {
+            reverse-proxy patchouli:2020
+          }
+      '';
+    };
   };
 }
