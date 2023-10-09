09b3b9225153a877c70aa5f6cf4af6b5298345ec
bromine1 <rwstauffer72@gmail.com>
2023-10-09 15:47:53 -0400

    fix nginx names for web servers

--- modules/rss.nix
+++ modules/rss.nix
@@ -4,25 +4,32 @@
   };
   services.freshrss = {
     passwordFile = config.sops.secrets.freshrss.path;
-    baseUrl = "https://patchouli:80";
+    baseUrl = "";
     enable = true;
     defaultUser = "admin";
+    virtualHost = "freshrss.*";
   };
-  services.nginx.virtualHosts."freshrss".listen = [
-    {
-      port = 2020;
-      addr = "127.0.0.1";
-    }
-  ];
+  #services.nginx.virtualHosts."freshrss.patchouli.taild56d5.ts.net/*".listen = [
+  #  {
+  #    port = 2020;
+  #    addr = "0.0.0.0";
+  #  }
+  #];
 
+  #services.nginx.virtualHosts."freshrss.patchouli.taild56d5.ts.net" = {
+  #sslCertificateKey = "/var/patchouli.taild56d5.ts.net.key";
+  #sslCertificate = "/var/patchouli.taild56d5.ts.net.crt";
+  #};
   services.rss-bridge = {
     enable = true;
     whitelist = ["*"];
+    virtualHost = "rss-bridge.*";
   };
-  services.nginx.virtualHosts."rss-bridge".listen = [
-    {
-      port = 2021;
-      addr = "127.0.0.1";
-    }
-  ];
+  #services.nginx.virtualHosts."rss-bridge".listen = [
+  #  {
+  #    port = 2021;
+  #    addr = "0.0.0.0";
+  #  }
+  #];
 }
+ryan@patch
