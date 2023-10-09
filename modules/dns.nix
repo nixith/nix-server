a13461ea7007c15bd4675ee3e84bb84c9a0f6478
bromine1 <rwstauffer72@gmail.com>
2023-10-09 15:47:12 -0400

    update dns module to use reolved

--- modules/dns.nix
+++ modules/dns.nix
@@ -1,43 +1,18 @@
 {...}: {
-  services.unbound = {
+  services.resolved = {
     enable = true;
-    resolveLocalQueries = true;
-    settings = {
-      server = {
-        interface = ["127.0.0.1"];
-        port = "5335";
-        do-ip4 = "yes";
-        do-udp = "yes";
-        do-tcp = "yes";
-        harden-glue = "yes";
-        edns-buffer-size = 1232;
-        prefetch = true;
-        private-address = [
-          "192.168.0.0/16"
-          "169.254.0.0/16"
-          "172.16.0.0/12"
-          "10.0.0.0/8"
-          "fd00::/8"
-          "fe80::/10"
-        ];
-      };
-      forward-zone = [
-        {
-          name = ".";
-          forward-addr = [
-            "1.1.1.1"
-            "8.8.8.8"
-          ];
-        }
-
-        {
-          name = "patchouli.me.";
-          forward-addr = [
-            "127.0.0.1"
-          ];
-        }
-      ];
-      #remote-control.control-enable = true;
-    };
+    extraConfig = ''
+      ReadEtcHosts=yes
+      DNSStubListenerExtra=100.88.114.3
+    '';
   };
+
+  networking.networkmanager.dns = "systemd-resolved";
+
+  networking.extraHosts = ''
+    100.88.114.3 patchouli.ts.net
+    100.88.114.3 calibre.patchouli.ts.net
+    100.88.114.3 freshrss.patchouli.ts.net
+    100.88.114.3 rss-bridge.patchouli.ts.net
+  '';
 }
