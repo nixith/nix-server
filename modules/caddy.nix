c2f1d746a5e94272f6d4c2f71da33b318adc03c4
nix <rwstauffer72@gmail.com>
2024-01-06 16:57:05 -0500

    we'll get there eventually

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -23,28 +23,25 @@
       debug
     '';
     logFormat = "level INFO";
-    virtualHosts."patchouli.nixith.dev" = {
-      # have to set rss-bridge as root for now, waiting on subdomains from tailscale
+    virtualHosts."nixith.dev" = {
       #
       extraConfig = ''
          tls {
              dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+               resolvers 1.1.1.1
          }
-        reverse_proxy /rss/* localhost:8080
-        reverse_proxy :8083
-        reverse_proxy /calibre/lib/* :8082
-        php_fastcgi unix//run/phpfpm/rss-bridge.sock
+        reverse_proxy localhost:8080
       '';
       #hostName = "freshrss";
     };
-    virtualHosts."calibre.patchouli.centaur-stargazer.ts.net" = {
-      extraConfig = ''
-        tls {
-          get_certificate tailscale
-        }
-        reverse_proxy :8083
-      '';
-      #hostName = "freshrss";
-    };
+    # virtualHosts."calibre.patchouli.centaur-stargazer.ts.net" = {
+    #   extraConfig = ''
+    #     tls {
+    #       get_certificate tailscale
+    #     }
+    #     reverse_proxy :8083
+    #   '';
+    #hostName = "freshrss";
+    # };
   };
 }
